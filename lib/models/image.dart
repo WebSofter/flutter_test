b359eca5cc753205787936220ab3f257745a46f4
David Amirkhanov <mail.websofter@gmail.com>
2022-08-30 09:32:48 +0300

    Add screens

--- lib/models/image.dart
+++ lib/models/image.dart
@@ -1,12 +1,14 @@
 class ImageModel {
+  late int albumId;
   late int id;
   late String title;
   late String url;
 
-  ImageModel(this.id, this.url, this.title);
+  ImageModel(this.albumId, this.id, this.url, this.title);
 
   ImageModel.fromJson(Map<String, dynamic> parsedJson) {
     this
+      ..albumId = parsedJson['albumId']
       ..id = parsedJson['id']
       ..url = parsedJson['url']
       ..title = parsedJson['title'];
